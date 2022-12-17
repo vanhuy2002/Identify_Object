469b4e7d06f68424964ff7241d03c0e60ee3510a
nhuquynh2603 <nnhuquynh2603@gmail.com>
2022-12-18 02:37:42 +0700

    no message

--- app/src/main/java/com/example/identify_object/ResultActivity.java
+++ app/src/main/java/com/example/identify_object/ResultActivity.java
@@ -3,13 +3,24 @@ package com.example.identify_object;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ResultActivity extends AppCompatActivity {
-
+    ImageButton btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 }
 
