0724422abf2fdb09269449b740bd05024539a017
maivanhuy202 <vanhuy112002qn@gmail.com>
2022-12-18 00:08:12 +0700

    fix scan

---
+++ app/src/main/java/com/example/identify_object/ResultActivity.java
@@ -0,0 +1,6 @@
+package com.example.identify_object;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/example/identify_object/ScanFragment.java
+++ app/src/main/java/com/example/identify_object/ScanFragment.java
@@ -1,8 +1,13 @@
 package com.example.identify_object;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.util.Size;
 import android.view.LayoutInflater;
@@ -14,15 +19,18 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.Preview;
-import androidx.camera.core.impl.ImageCaptureConfig;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
 
 import com.example.identify_object.utils.Draw;
 import com.google.common.util.concurrent.ListenableFuture;
@@ -33,37 +41,68 @@ import com.google.mlkit.vision.objects.ObjectDetection;
 import com.google.mlkit.vision.objects.ObjectDetector;
 import com.google.mlkit.vision.objects.custom.CustomObjectDetectorOptions;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 public class ScanFragment extends Fragment {
 
     ImageView btn_gallery, btn_take, btn_flash;
+
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     PreviewView previewView;
     ImageCapture imageCapture;
-    private ObjectDetector objectDetector;
 
+    private ObjectDetector objectDetector;
+    Camera camera;
+    private static final int PERMISSION_REQUEST_CAMERA = 101;
+    View view;
+    Preview preview;
+    CameraSelector cameraSelector;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_scan, container, false);
+        view = inflater.inflate(R.layout.fragment_scan, container, false);
 
         btn_flash = view.findViewById(R.id.btn_flash);
         btn_gallery = view.findViewById(R.id.btn_gallary);
         btn_take = view.findViewById(R.id.btn_take);
         previewView = view.findViewById(R.id.preview);
 
-        cameraProviderFuture = ProcessCameraProvider.getInstance(getContext());
-        cameraProviderFuture.addListener(() -> {
-            try {
-                ProcessCameraProvider processCameraProvider = cameraProviderFuture.get();
-                bindPreview(processCameraProvider, view);
-            } catch (ExecutionException | InterruptedException e) {
-                e.printStackTrace();
-            }
-        }, ContextCompat.getMainExecutor(getContext()));
 
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+        } else {
+            initializeCamera();
+        }
+
+//        cameraProviderFuture = ProcessCameraProvider.getInstance(getContext());
+//        //Build preview
+//        Preview preview = new Preview.Builder().build();
+//        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+//        //Build camera
+//        CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+//
+//        cameraProviderFuture.addListener(() -> {
+//            try {
+//                ProcessCameraProvider processCameraProvider = cameraProviderFuture.get();
+//                imageCapture = new ImageCapture.Builder().build();
+//                try {
+//                    processCameraProvider.unbindAll();
+//                    processCameraProvider.bindToLifecycle(requireActivity(), cameraSelector, preview, imageCapture);
+//                }catch (Exception e) {
+//                    Log.e("TAG", "Use case binding failed");
+//                }
+//                //bindPreview(processCameraProvider, view);
+//            } catch (ExecutionException | InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }, ContextCompat.getMainExecutor(getContext()));
+//
         LocalModel localModel = new LocalModel.Builder().setAssetFilePath("object_detection.tflite").build();
         CustomObjectDetectorOptions customObjectDetectorOptions = new CustomObjectDetectorOptions.Builder(localModel)
                 .setDetectorMode(CustomObjectDetectorOptions.STREAM_MODE)
@@ -72,19 +111,18 @@ public class ScanFragment extends Fragment {
                 .setMaxPerObjectLabelCount(3)
                 .build();
         objectDetector = ObjectDetection.getClient(customObjectDetectorOptions);
+
+        btn_take.setOnClickListener(take -> {
+            try {
+                capturePhoto();
+            } catch (IOException | ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
         return view;
     }
 
     private void bindPreview(ProcessCameraProvider cameraProvider, View view) {
-        //Build preview
-        Preview preview = new Preview.Builder().build();
-        //Build camera
-        CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
-        preview.setSurfaceProvider(previewView.getSurfaceProvider());
-
-        //Build image capture
-        ImageCapture imageCapture = new ImageCapture.Builder().setCaptureMode(ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY).build();
-        //Build image analysis
         ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
                 .setTargetResolution(new Size(1280,720))
                 .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
@@ -100,17 +138,16 @@ public class ScanFragment extends Fragment {
                 InputImage inputImage = InputImage.fromMediaImage(image,rotation);
                 objectDetector.process(inputImage)
                         .addOnSuccessListener(object -> {
-                            RelativeLayout relativeLayout = view.findViewById(R.id.parent_layout);
                             for (DetectedObject i : object ){
 //                                if (relativeLayout.getChildCount() > 1) relativeLayout.removeViewAt(1);
-//                                String txt = "Undefined";
-//                                if (i.getLabels().size() != 0)
-//                                    txt = i.getLabels().get(0).getText().toString();
-//
-//                                Draw draw = new Draw(getContext(),i.getBoundingBox()
-//                                        ,txt);
-//
-//                                relativeLayout.addView(draw);
+                                String txt = "Undefined";
+                                if (i.getLabels().size() != 0)
+                                    txt = i.getLabels().get(0).getText().toString();
+
+                                Draw draw = new Draw(getContext(),i.getBoundingBox()
+                                        ,txt);
+
+                                previewView.addView(draw);
                             }
 
                             imageProxy.close();
@@ -125,7 +162,91 @@ public class ScanFragment extends Fragment {
 
         cameraProvider.bindToLifecycle(this, cameraSelector, imageAnalysis,preview);
     }
-    private void takePhoto(){
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    private void initializeCamera() {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(getContext());
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                startCameraX(cameraProvider);
+
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, getExecuter());
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CAMERA && grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            initializeCamera();
+        }
+    }
+    private void capturePhoto() throws IOException, ExecutionException, InterruptedException {
+        ProcessCameraProvider processCameraProvider = cameraProviderFuture.get();
+        bindPreview(processCameraProvider, view);
+        File photoFile = createImageFile();
+        imageCapture.takePicture(
+                new ImageCapture.OutputFileOptions.Builder(photoFile).build(),
+                getExecuter(),
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                        Intent intent = new Intent(getContext(), ResultActivity.class);
+                        Uri contentUri = Uri.fromFile(photoFile);
+                        intent.putExtra("photoUri", contentUri.toString());
+                        Log.e("PathMVH", contentUri.getPath());
+                        startActivity(intent);
+
+                        Toast.makeText(getContext(), "Photo has been saved successfully", Toast.LENGTH_SHORT).show();
+                    }
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception) {
+                        Toast.makeText(getContext(), "Error saving photo: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
 
     }
+    private File createImageFile() throws IOException {
+        Date date = new Date();
+        String timestamp = String.valueOf(date.getTime());
+        String imageFileName = "JPEG_" + timestamp + "_";
+        File storageDir = getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        //        currentPhotoPath = image.getAbsolutePath();
+        return File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+    }
+    private Executor getExecuter() {
+        return ContextCompat.getMainExecutor(getContext());
+    }
+    private void startCameraX(ProcessCameraProvider cameraProvider) {
+        cameraProvider.unbindAll();
+//Camera Selector use case
+        cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+//Preview use case
+        preview = new Preview.Builder().build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+//ImageCapture use case
+        imageCapture = new ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                .build();
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, imageCapture);
+    }
+
 }
